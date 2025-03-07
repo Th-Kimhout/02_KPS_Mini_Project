1d4e6bcedf2d7a060ba682b416b1259d538067cd
Th-Kimhout <th.kimhout@gmail.com>
2025-03-07 17:12:22 +0700

    test connection

--- src/main/java/org/app/Main.java
+++ src/main/java/org/app/Main.java
@@ -1,8 +1,13 @@
 package org.app;
 
+import org.app.utilies.DBConfig;
+
 public class Main {
     public static void main(String[] args) {
 
-
+        try {
+            System.out.println(DBConfig.getConnection().isValid(5));
+        } catch (Exception e) {
+        }
     }
 }
--- src/main/java/org/app/utilies/CredentialLoader.java
+++ src/main/java/org/app/utilies/CredentialLoader.java
@@ -7,7 +7,7 @@ public class CredentialLoader {
     public static Properties properties = new Properties();
 
     public static void loadProperties() {
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("src/application.properties"))) {
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader("application.properties"))) {
             properties.load(bufferedReader);
         } catch (Exception e) {
             System.out.println(e.getMessage());
