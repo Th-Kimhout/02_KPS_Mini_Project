5e6f969c5a2e59990cc71cdd1b629024dd2cc022
NarinHorn <hornnarin9@gmail.com>
2025-03-07 22:25:00 +0700

    test push

--- src/main/java/org/app/Main.java
+++ src/main/java/org/app/Main.java
@@ -4,7 +4,7 @@ import org.app.utilies.DBConfig;
 
 public class Main {
     public static void main(String[] args) {
-
+        hello
         try {
             System.out.println(DBConfig.getConnection().isValid(5));
         } catch (Exception e) {
